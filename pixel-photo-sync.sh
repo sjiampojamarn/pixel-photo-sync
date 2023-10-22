62f17e4fc8021dbc192b1f2a8d3fefd801bbfe91
sjiampojamarn <18257803+sjiampojamarn@users.noreply.github.com>
2023-10-21 22:00:36 -0700

    Create pixel-photo-sync.sh

---
+++ pixel-photo-sync.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+## Initialize folders
+mkdir -p /files/PhotosSynced/
+mkdir -p /files/PhotosPending/
+
+shopt -s globstar
+
+while true; do
+  timeout 15m inotifywait -e modify,create,delete -r /files/Photos 
+  echo "Waiting for 1m ..."
+  sleep 1m
+  
+  ## construct files that are already synced to skip.
+  ls -1v /files/PhotosSynced/**/*.* | xargs -n 1 -d '\n' basename > /files/alreadySynced.txt
+  ls -1v /PhotosArchive/**/*.* | xargs -n 1 -d '\n' basename >> /files/alreadySynced.txt
+  echo '*.part' >> /files/alreadySynced.txt
+  cp /files/alreadySynced.txt /files/PhotosPending/.stignore
+
+  ## remove files already synced from Pending.
+  while read -r file ; do rm -f "/files/PhotosPending/$file" ; done < /files/alreadySynced.txt
+
+  ## sync files from source; ignore those already synced.
+  nice rsync -avXAh --progress --delete --delete-excluded --bwlimit=15000 --exclude-from /files/alreadySynced.txt /files/Photos/**/*.* /files/PhotosPending/
+
+  ## Some Live photo mov files having problems,
+  ## re-encode those older than 1 days and less than 10MB
+  mkdir -p /files/PhotosPending-mov-fix
+
+  OIFS="$IFS"
+  IFS=$'\n'
+  for file in `find /files/PhotosPending/ -mtime +1 -size -10M -type f -name "*.mov"` ;
+  do
+    echo "Fixing: ${file}" ;
+    cp -p "$file" /files/PhotosPending-mov-fix/in.mov ;
+    ffmpeg -hide_banner -loglevel warning -i /files/PhotosPending-mov-fix/in.mov -c copy -map_metadata 0 -map_metadata:s:v 0:s:v -map_metadata:s:a 0:s:a -movflags use_metadata_tags -y /files/PhotosPending-mov-fix/out.mov ;
+    chown --reference=/files/PhotosPending-mov-fix/in.mov /files/PhotosPending-mov-fix/out.mov ;
+    chmod --reference=/files/PhotosPending-mov-fix/in.mov /files/PhotosPending-mov-fix/out.mov ;
+    touch --reference=/files/PhotosPending-mov-fix/in.mov /files/PhotosPending-mov-fix/out.mov ;
+    mv /files/PhotosPending-mov-fix/out.mov "$file" ;
+    rm /files/PhotosPending-mov-fix/in.mov ;
+  done
+  IFS="$OIFS"
+
+  ls -1v /files/PhotosSynced/**/*.* | xargs -n 1 -d '\n' basename > /files/PhotosSynced.txt
+  ls -1v /PhotosArchive/**/*.* | xargs -n 1 -d '\n' basename >> /files/PhotosSynced.txt
+
+  ls -1v /files/PhotosPending/**/*.* | xargs -n 1 -d '\n' basename > /files/PhotosPending.txt
+  ls -1v /files/Photos/**/*.* | xargs -n 1 -d '\n' basename > /files/Photos.txt
+
+  sort /files/PhotosSynced.txt > /files/PhotosSynced.txt.sort
+  sort /files/PhotosPending.txt > /files/PhotosPending.txt.sort
+  sort /files/Photos.txt > /files/Photos.txt.sort
+
+  comm -13 <(sort /files/Photos.txt.sort) <(sort /files/PhotosSynced.txt.sort) > /files/lostFiles.txt
+  comm -23 <(sort /files/Photos.txt.sort) <(sort /files/PhotosSynced.txt.sort) > /files/missingFiles.txt
+
+  wc /files/Photos.txt.sort
+  wc /files/PhotosPending.txt.sort
+  wc /files/PhotosSynced.txt.sort
+
+  wc /files/alreadySynced.txt
+  wc /files/PhotosPending/.stignore
+
+  set -x
+
+  wc /files/lostFiles.txt
+  head /files/lostFiles.txt
+  tail /files/lostFiles.txt
+
+    wc /files/missingFiles.txt
+  head /files/missingFiles.txt
+  tail /files/missingFiles.txt
+
+  set +x
+
+  ## Move files older than 7 days from Synced to Archive
+  find /files/PhotosSynced/ -mtime +7 | cut -f4- -d/ |sort > /files/moveToArchive.txt
+  rsync -avXAh --progress --remove-source-files --files-from /files/moveToArchive.txt /files/PhotosSynced/ /PhotosArchive/ 
+
+  ## doing the same from Photos to Archive
+  cat /files/alreadySynced.txt | xargs -n 1 -d '\n' find /files/Photos/ -mtime +7 -name |  cut -f4- -d/ | sort > /files/photosMoveToArchive.txt
+  rsync -avXAh --progress --remove-source-files --files-from /files/photosMoveToArchive.txt /files/Photos/ /PhotosArchive/
+
+  set -x
+
+  wc /files/lostFiles.txt
+
+  wc /files/missingFiles.txt
+  head /files/missingFiles.txt
+  tail /files/missingFiles.txt
+
+  set +x
+
+done
+  
