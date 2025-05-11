205947aa3df737e0d87838f45d0ee7be0701ef13
sjiampojamarn <18257803+sjiampojamarn@users.noreply.github.com>
2025-05-11 10:55:21 -0700

    preventing one-off error to 0m

--- pixel-photo-sync.sh
+++ pixel-photo-sync.sh
@@ -8,7 +8,7 @@ shopt -s globstar
 
 while true; do
   date
-  timeout $(($RANDOM % 15))m inotifywait -e modify,create,delete -r /files/Photos 
+  timeout $((RANDOM % 15 + 1))m inotifywait -e modify,create,delete -r /files/Photos 
   echo "Waiting for 1m ..."
   sleep 1m
   
